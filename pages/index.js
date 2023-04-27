354273a41df0675ff0e2f2d50e57f7489f2f2d28
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-27 20:22:03 +0530

    initial commit...

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const index = () => {
+  return (
+    <div className="h-screen bg-stone-900">
+      <h1 className="text-3xl font-semibold text-center pt-10 text-purple-700">
+        Hello from my ecomm app
+      </h1>
+    </div>
+  );
+};
+
+export default index;
